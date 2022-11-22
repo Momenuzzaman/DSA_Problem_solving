e1a31cbc17c29044ddd7693adb653fa02ad991fb
mdemon420 <mdeamon4@gmail.com>
2022-11-22 11:34:27 +0600

    done

---
+++ SearchInsertPosition.js
@@ -0,0 +1,22 @@
+const nums = [1, 3, 5, 6];
+const target = 2;
+
+const searchInsert = function (nums, target) {
+    let start = 0;
+    let end = nums.length - 1;
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        }
+        else if (nums[mid] < target) {
+            start = mid + 1;
+        }
+        else {
+            end = mid - 1;
+        }
+    }
+    return start;
+};
+
+console.log(searchInsert(nums, target));
