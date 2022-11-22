1ed76f653d33e450dec98f643c4aa95467076bd8
mdemon420 <mdeamon4@gmail.com>
2022-11-22 20:16:59 +0600

    flooring.js

---
+++ flooring.js
@@ -0,0 +1,23 @@
+const array = [10, 20, 30, 40, 50, 60, 70];
+const target = 55;
+
+const flooring = (array, target) => {
+    let start = 0;
+    let end = array.length - 1;
+    let mid;
+    while (start <= end) {
+        mid = Math.floor((start + end) / 2);
+        if (array[mid] === target) {
+            return mid;
+        }
+        if (array[mid] < target) {
+            start = mid + 1;
+        }
+        else {
+            end = mid - 1;
+        }
+    }
+    return mid - 1;
+};
+
+console.log(flooring(array, target));
