6daec31abe65b2a3772a0dfa04002313f38d228e
mdemon420 <mdeamon4@gmail.com>
2022-12-08 00:55:02 +0600

    problem 1295

---
+++ evenNumber1295.js
@@ -0,0 +1,9 @@
+const findNumbers = (array) => {
+    let count = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            count++;
+        }
+    }
+    return count;
+};
