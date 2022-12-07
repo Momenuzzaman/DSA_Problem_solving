1acd5b8c9f5fcb58db4e1061aecdd6bd2ec82a03
mdemon420 <mdeamon4@gmail.com>
2022-12-08 01:16:54 +0600

    problem1929

---
+++ concatenation1929.js
@@ -0,0 +1,18 @@
+const getConcatenation = (nums) => {
+    let ans = [];
+    for (let i = 0; i < nums.length; i++) {
+        ans.push(nums[i]);
+        if (nums.length === ans.length) {
+            for (let j = 0; j < nums.length; j++) {
+                ans.push(ans[j]);
+            }
+        }
+    }
+    return ans;
+};
+
+console.log(getConcatenation([1, 2, 1]));
+
+const getConcatenation2 = (nums) => {
+    return nums.concat(nums);
+};
--- evenNumber1295.js
+++ evenNumber1295.js
@@ -1,9 +1,12 @@
 const findNumbers = (array) => {
     let count = 0;
     for (let i = 0; i < array.length; i++) {
-        if (array[i] % 2 === 0) {
+        if (array[i].toString().length % 2 === 0) {
             count++;
         }
     }
     return count;
 };
+
+console.log(findNumbers([12, 345, 2, 6, 7896]));
+console.log(findNumbers([555, 901, 482, 1771]));
