cd41435b0a532a69ccb75f067bed02badd6f29a6
mdemon420 <mdeamon4@gmail.com>
2023-01-07 02:18:25 +0600

    array reverse

--- Recursion/recursion.js
+++ Recursion/recursion.js
@@ -1,64 +1,83 @@
-const abc = (number) => {
-    if (number === 0) {
+// const abc = (number) => {
+//     if (number === 0) {
+//         return;
+//     }
+//     console.log(number);
+//     abc(number - 1);
+// };
+// abc(10);
+
+
+// const abc2 = (number) => {
+//     if (number === 10) {
+//         return;
+//     }
+//     console.log(number);
+//     abc2(number + 1);
+// };
+// abc2(1);
+
+// // factorial
+// const fac = (number) => {
+//     if (number === 1) {
+//         return 1;
+//     }
+//     return number * fac(number - 1);
+// };
+// console.log(fac(5));
+
+
+
+// const sum = (number) => {
+//     let result = 0;
+//     if (number > 6) {
+//         return;
+//     }
+//     result = result + number;
+//     sum(number + 1);
+//     return result;
+// };
+// console.log(sum(1));
+
+// // array traverse 
+// const array = [1, 2, 3, 4, 5];
+// const traverse = (array, i) => {
+//     if (array.length == i) {
+//         return;
+//     }
+//     console.log(array[i]);
+//     traverse(array, i + 1);
+// };
+// traverse(array, 0);
+
+// // sum 
+// let sumValue = 0;
+// const summation = (array, i) => {
+//     if (array.length == i) {
+//         return;
+//     }
+//     sumValue = sumValue + array[i];
+//     summation(array, i + 1);
+//     return sumValue;
+// };
+// console.log(summation(array, 0));
+
+// array reverse 
+
+const array = [10, 9, 8, 7, 6, 4, 3, 2, 1];
+const array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const reverse = (array, i, j) => {
+    if (i >= j) {
         return;
     }
-    console.log(number);
-    abc(number - 1);
+    // let tem = array[i];
+    // array[i] = array[j];
+    // array[j] = tem;
+    [array[i], array[j]] = [array[j], array[i]];
+    reverse(array, i + 1, j - 1);
+    return array;
 };
-abc(10);
-
-
-const abc2 = (number) => {
-    if (number === 10) {
-        return;
-    }
-    console.log(number);
-    abc2(number + 1);
-};
-abc2(1);
-
-// factorial
-const fac = (number) => {
-    if (number === 1) {
-        return 1;
-    }
-    return number * fac(number - 1);
-};
-console.log(fac(5));
-
-
-
-const sum = (number) => {
-    let result = 0;
-    if (number > 6) {
-        return;
-    }
-    result = result + number;
-    sum(number + 1);
-    return result;
-};
-console.log(sum(1));
-
-// array traverse 
-const array = [1, 2, 3, 4, 5];
-const traverse = (array, i) => {
-    if (array.length == i) {
-        return;
-    }
-    console.log(array[i]);
-    traverse(array, i + 1);
-};
-traverse(array, 0);
-
-// sum 
-let sumValue = 0;
-const summation = (array, i) => {
-    if (array.length == i) {
-        return;
-    }
-    sumValue = sumValue + array[i];
-    summation(array, i + 1);
-    return sumValue;
-};
-console.log(summation(array, 0));
-
+console.log(reverse(array, 0, array.length - 1));
+console.log(reverse(array2, 0, array.length - 1));
+const arrayReverse = array2.reverse();
+console.log(arrayReverse)
