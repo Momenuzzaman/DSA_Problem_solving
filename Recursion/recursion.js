6e0591acd61ace0434fda15f2988b1900c1b8532
mdemon420 <mdeamon4@gmail.com>
2023-01-07 01:37:53 +0600

    array traverse

--- Recursion/recursion.js
+++ Recursion/recursion.js
@@ -1,8 +1,52 @@
 const abc = (number) => {
+    if (number === 0) {
+        return;
+    }
+    console.log(number);
+    abc(number - 1);
+};
+abc(10);
+
+
+const abc2 = (number) => {
     if (number === 10) {
         return;
     }
     console.log(number);
-    abc(number + 1);
+    abc2(number + 1);
 };
-abc(1);
+abc2(1);
+
+// factorial
+const fac = (number) => {
+    if (number === 1) {
+        return 1;
+    }
+    return number * fac(number - 1);
+};
+console.log(fac(5));
+
+
+
+const sum = (number) => {
+    let result = 0;
+    if (number > 6) {
+        return;
+    }
+    result = result + number;
+    sum(number + 1);
+    return result;
+};
+console.log(sum(1));
+
+// array traverse 
+const array = [1, 2, 3, 4, 5];
+const traverse = (array, i) => {
+    if (array.length == i) {
+        return;
+    }
+    console.log(array[i]);
+    traverse(array, i + 1);
+};
+
+traverse(array, 0);
