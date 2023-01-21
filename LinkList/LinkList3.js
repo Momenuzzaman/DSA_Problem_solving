f16093d54598e6b4c3ec2acaf558e852a42bfbf1
mdemon420 <mdeamon4@gmail.com>
2023-01-22 01:22:50 +0600

    start day-3

---
+++ LinkList/LinkList3.js
@@ -0,0 +1,67 @@
+
+class Node {
+    constructor(value, next = null) {
+        this.value = value;
+        this.next = next;
+    }
+}
+
+class LinkList {
+    constructor(value) {
+        let node = new Node(value);
+        this.head = node;
+        this.tail = node;
+        this.tail.next = this.head
+        this.length = 1;
+    }
+    append(value) {
+        let node = new Node(value);
+        this.tail.next = node;
+        this.tail = node;
+        this.tail.next = this.head
+        this.length++;
+    }
+    prepend(value) {
+        let node = new Node(value);
+        node.next = this.head;
+        this.head = node;
+        this.length++;
+    }
+    print() {
+        let data = this.head;
+        while (data) {
+            console.log(data.value);
+            data = data.next;
+        }
+    }
+    appendAt(value, position) {
+        if (position === 1) {
+            this.prepend(value);
+        }
+        else if (position === this.length + 1) {
+            this.append(value);
+        }
+        else {
+            let newNode = new Node(value);
+            let preNode = this.findNode(position - 1);
+            newNode.next = preNode.next;
+            preNode.next = newNode;
+
+            this.length++;
+        }
+    }
+    findNode(n) {
+        let count = 1;
+        let data = this.head;
+        while (data) {
+            if (count === n) break;
+            count++;
+            data = data.next;
+        }
+        return data;
+    }
+}
+
+const list = new LinkList(2);
+list.append(3)
+console.log(list.head)
